ff7fc6b5bc5068a66a62d791cbbb46bdfa8ffcce
Joel Monné <joel.monne@gmail.com>
2016-03-14 11:35:49 +0100

    search screen fixed

--- app/src/main/java/plataformesenxarxa/marcclua/joelmonne/festespopulars/Search.java
+++ app/src/main/java/plataformesenxarxa/marcclua/joelmonne/festespopulars/Search.java
@@ -1,7 +1,13 @@
 package plataformesenxarxa.marcclua.joelmonne.festespopulars;
 
-/**
- * Created by Joel Monné on 14/03/2016.
- */
-public class Search {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cerca);
+    }
 }
