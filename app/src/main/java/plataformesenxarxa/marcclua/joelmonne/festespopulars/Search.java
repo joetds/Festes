7638198cbcb179a74514881c2af75bd0f453f318
Joel Monné <joel.monne@gmail.com>
2016-04-05 12:10:43 +0200

    Return button added in search screen

--- app/src/main/java/plataformesenxarxa/marcclua/joelmonne/festespopulars/Search.java
+++ app/src/main/java/plataformesenxarxa/marcclua/joelmonne/festespopulars/Search.java
@@ -1,13 +1,19 @@
 package plataformesenxarxa.marcclua.joelmonne.festespopulars;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class Search extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cerca);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
