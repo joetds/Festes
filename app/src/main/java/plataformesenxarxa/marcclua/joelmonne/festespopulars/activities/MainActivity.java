2ba76d598f7aba81169b59aad9b1cde2a68ebd7f
Joel Monné <joel.monne@gmail.com>
2016-05-22 03:04:53 +0200

    Put main view as fragment

--- app/src/main/java/plataformesenxarxa/marcclua/joelmonne/festespopulars/activities/MainActivity.java
+++ app/src/main/java/plataformesenxarxa/marcclua/joelmonne/festespopulars/activities/MainActivity.java
@@ -1,48 +1,28 @@
 package plataformesenxarxa.marcclua.joelmonne.festespopulars.activities;
 
-import android.app.DatePickerDialog;
-import android.content.Context;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.festespopulars.backend.festespopularsAPI.FestespopularsAPI;
-import com.example.festespopulars.backend.festespopularsAPI.model.EventBean;
-import com.example.festespopulars.backend.messaging.Messaging;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-
-import java.io.IOException;
-import java.util.Calendar;
 
 import plataformesenxarxa.marcclua.joelmonne.festespopulars.R;
+import plataformesenxarxa.marcclua.joelmonne.festespopulars.fragments.SearchFragment;
 import plataformesenxarxa.marcclua.joelmonne.festespopulars.messaging.MessagingPreferences;
 import plataformesenxarxa.marcclua.joelmonne.festespopulars.messaging.RegistrationIntentService;
-import plataformesenxarxa.marcclua.joelmonne.festespopulars.models.Event;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -50,77 +30,13 @@ public class MainActivity extends AppCompatActivity
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private static final String TAG = "MainActivity";
 
-    private Context context;
-    private View main_content_view;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        main_content_view = findViewById(R.id.main_content);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        context = MainActivity.this;
         configureMessaging();
-        final Button button_search = (Button) findViewById(R.id.boto_cerca);
-        button_search.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                EditText text = (EditText) findViewById(R.id.search_text);
-                String searchText = text.getText().toString();
-                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
-                intent.putExtra("search", searchText);
-                startActivity(intent);
-            }
-        });
-
-        final Button button_event = (Button) findViewById(R.id.anar_esdeveniment);
-        button_event.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EventActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_event_button);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final View dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_create_event, null);
-                final AlertDialog dialog = new AlertDialog.Builder(context)
-                        .setView(dialogView).create();
-                dialogView.findViewById(R.id.date).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(final View v) {
-                        final Calendar c = Calendar.getInstance();
-                        int year = c.get(Calendar.YEAR);
-                        int month = c.get(Calendar.MONTH);
-                        int day = c.get(Calendar.DAY_OF_MONTH);
-                        new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                ((TextView) v).setText(dayOfMonth + "/" + monthOfYear + "/" + year);
-                            }
-                        }, year, month, day).show();
-                    }
-                });
-                dialogView.findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (saveEvent(dialogView)) {
-                            dialog.dismiss();
-                        }
-                    }
-                });
-                dialogView.findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
-                dialog.show();
-
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -130,83 +46,18 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        navigationView.getMenu().getItem(0).setChecked(true);
+        onNavigationItemSelected(navigationView.getMenu().getItem(0));
     }
 
-    private void showToast(String text) {
-        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
-    }
-
-    private boolean saveEvent(View eventView) {
-        Event event = checkFields(eventView);
-        if (event != null) {
-            new AsyncTask<EventBean, Void, Void>() {
-                @Override
-                protected Void doInBackground(EventBean... params) {
-                    FestespopularsAPI.Builder builder = new FestespopularsAPI.Builder(AndroidHttp.newCompatibleTransport(),
-                            new AndroidJsonFactory(), null);
-                    FestespopularsAPI api = builder.build();
-                    try {
-                        api.storeEvent(params[0]).execute();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                }
-
-                @Override
-                protected void onPostExecute(Void aVoid) {
-                    super.onPostExecute(aVoid);
-                    Snackbar.make(main_content_view, getString(R.string.event_saved), Snackbar.LENGTH_LONG)
-                            .setAction("Undo", new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    new AsyncTask<Void, Void, Void>() {
-                                        @Override
-                                        protected Void doInBackground(Void... params) {
-                                            Messaging.Builder builder = new Messaging.Builder(AndroidHttp.newCompatibleTransport(),
-                                                    new AndroidJsonFactory(), null);
-                                            Messaging messaging = builder.build();
-                                            try {
-                                                messaging.messagingEndpoint().sendMessage("Ja funcionaaaaaa!!!").execute();
-                                            } catch (IOException e) {
-                                                e.printStackTrace();
-                                            }
-                                            return null;
-                                        }
-                                    }.execute();
-                                }
-                            }).show();
-                }
-            }.execute(Event.eventToEventBean(event));
-            return true;
+    public void loadFragment(int resId, Fragment fragment) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        if (fragmentManager.findFragmentById(resId) == null) {
+            transaction.add(resId, fragment);
         } else {
-            showToast("Check empty fields");
+            transaction.replace(resId, fragment);
         }
-        return false;
-    }
-
-    private Event checkFields(View eventView) {
-        Event event = new Event();
-
-        String name = ((TextView) eventView.findViewById(R.id.name)).getText().toString();
-        if (name.equals("")) return null;
-        event.setName(name);
-
-        String description = ((TextView) eventView.findViewById(R.id.description)).getText().toString();
-        event.setDescription(description);
-
-        String place = ((TextView) eventView.findViewById(R.id.place)).getText().toString();
-        if (place.equals("")) return null;
-        event.setPlace(place);
-
-        String date = ((TextView) eventView.findViewById(R.id.date)).getText().toString();
-        if (date.equals("")) return null;
-        event.setDate(date);
-
-        event.setLocation(new LatLng(41.533333, 0.5166667));
-
-        return event;
+        transaction.commit();
     }
 
     @Override
@@ -219,36 +70,17 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        int resId = R.id.main_content;
+        Fragment fragment;
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_search) {
+            fragment = SearchFragment.getInstance();
+            loadFragment(resId, fragment);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
@@ -268,7 +100,7 @@ public class MainActivity extends AppCompatActivity
 
     private void configureMessaging() {
         SharedPreferences sharedPreferences =
-                PreferenceManager.getDefaultSharedPreferences(context);
+                PreferenceManager.getDefaultSharedPreferences(this);
         boolean sentToken = sharedPreferences
                 .getBoolean(MessagingPreferences.SENT_TOKEN_TO_SERVER, false);
         if (!sentToken) {
@@ -299,22 +131,5 @@ public class MainActivity extends AppCompatActivity
             return false;
         }
         return true;
-    }
-
-    public void testGCM(View view) {
-        new AsyncTask<Void, Void, Void>() {
-            @Override
-            protected Void doInBackground(Void... params) {
-                Messaging.Builder builder = new Messaging.Builder(AndroidHttp.newCompatibleTransport(),
-                        new AndroidJsonFactory(), null);
-                Messaging messaging = builder.build();
-                try {
-                    messaging.messagingEndpoint().sendMessage("Ja funcionaaaaaa!!!").execute();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-        }.execute();
     }
 }
---
+++ app/src/main/java/plataformesenxarxa/marcclua/joelmonne/festespopulars/fragments/SearchFragment.java
@@ -0,0 +1,198 @@
+package plataformesenxarxa.marcclua.joelmonne.festespopulars.fragments;
+
+import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.festespopulars.backend.festespopularsAPI.FestespopularsAPI;
+import com.example.festespopulars.backend.festespopularsAPI.model.EventBean;
+import com.example.festespopulars.backend.messaging.Messaging;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import plataformesenxarxa.marcclua.joelmonne.festespopulars.R;
+import plataformesenxarxa.marcclua.joelmonne.festespopulars.activities.SearchActivity;
+import plataformesenxarxa.marcclua.joelmonne.festespopulars.models.Event;
+
+public class SearchFragment extends Fragment {
+
+    private Context context;
+
+    public static SearchFragment getInstance() {
+        return new SearchFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        context = getActivity();
+        final Button button_search = (Button) getView().findViewById(R.id.boto_cerca);
+        button_search.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText text = (EditText) getView().findViewById(R.id.search_text);
+                String searchText = text.getText().toString();
+                Intent intent = new Intent(context, SearchActivity.class);
+                intent.putExtra("search", searchText);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) getView().findViewById(R.id.add_event_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final View dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_create_event, null);
+                final AlertDialog dialog = new AlertDialog.Builder(context)
+                        .setView(dialogView).create();
+                dialogView.findViewById(R.id.date).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(final View v) {
+                        final Calendar c = Calendar.getInstance();
+                        int year = c.get(Calendar.YEAR);
+                        int month = c.get(Calendar.MONTH);
+                        int day = c.get(Calendar.DAY_OF_MONTH);
+                        new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                ((TextView) v).setText(dayOfMonth + "/" + monthOfYear + "/" + year);
+                            }
+                        }, year, month, day).show();
+                    }
+                });
+                dialogView.findViewById(R.id.save).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (saveEvent(dialogView)) {
+                            dialog.dismiss();
+                        }
+                    }
+                });
+                dialogView.findViewById(R.id.cancel).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        });
+        getView().findViewById(R.id.test_gcm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... params) {
+                        Messaging.Builder builder = new Messaging.Builder(AndroidHttp.newCompatibleTransport(),
+                                new AndroidJsonFactory(), null);
+                        Messaging messaging = builder.build();
+                        try {
+                            messaging.messagingEndpoint().sendMessage("Ja funcionaaaaaa!!!").execute();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                }.execute();
+            }
+        });
+    }
+
+    private boolean saveEvent(View eventView) {
+        Event event = checkFields(eventView);
+        if (event != null) {
+            new AsyncTask<EventBean, Void, Void>() {
+                @Override
+                protected Void doInBackground(EventBean... params) {
+                    FestespopularsAPI.Builder builder = new FestespopularsAPI.Builder(AndroidHttp.newCompatibleTransport(),
+                            new AndroidJsonFactory(), null);
+                    FestespopularsAPI api = builder.build();
+                    try {
+                        api.storeEvent(params[0]).execute();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+                    Snackbar.make(getView(), getString(R.string.event_saved), Snackbar.LENGTH_LONG)
+                            .setAction("Undo", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    new AsyncTask<Void, Void, Void>() {
+                                        @Override
+                                        protected Void doInBackground(Void... params) {
+                                            Messaging.Builder builder = new Messaging.Builder(AndroidHttp.newCompatibleTransport(),
+                                                    new AndroidJsonFactory(), null);
+                                            Messaging messaging = builder.build();
+                                            try {
+                                                messaging.messagingEndpoint().sendMessage("Ja funcionaaaaaa!!!").execute();
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                            return null;
+                                        }
+                                    }.execute();
+                                }
+                            }).show();
+                }
+            }.execute(Event.eventToEventBean(event));
+            return true;
+        } else {
+            showToast("Check empty fields");
+        }
+        return false;
+    }
+
+    private Event checkFields(View eventView) {
+        Event event = new Event();
+
+        String name = ((TextView) eventView.findViewById(R.id.name)).getText().toString();
+        if (name.equals("")) return null;
+        event.setName(name);
+
+        String description = ((TextView) eventView.findViewById(R.id.description)).getText().toString();
+        event.setDescription(description);
+
+        String place = ((TextView) eventView.findViewById(R.id.place)).getText().toString();
+        if (place.equals("")) return null;
+        event.setPlace(place);
+
+        String date = ((TextView) eventView.findViewById(R.id.date)).getText().toString();
+        if (date.equals("")) return null;
+        event.setDate(date);
+
+        event.setLocation(new LatLng(41.533333, 0.5166667));
+
+        return event;
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+    }
+}
